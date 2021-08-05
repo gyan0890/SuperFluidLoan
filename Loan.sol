acc5b3e546b58e70c1a8df95228a66fe7ecaa81c
gyan0890 <gyanlakshmi@gmail.com>
2021-08-05 17:04:09 +0530

    Create Loan.sol

---
+++ Loan.sol
@@ -0,0 +1,94 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Loan {
+    address public lender;
+    address public borrower;
+    ERC20 public token;
+    uint256 public collateralAmount;
+    uint256 public payoffAmount;
+    uint256 public dueDate;
+    
+
+    constructor(
+        address _lender,
+        address _borrower,
+        ERC20 _token,
+        uint256 _collateralAmount,
+        uint256 _payoffAmount,
+        uint256 loanDuration
+    )
+    {
+        lender = _lender;
+        borrower = _borrower;
+        token = _token;
+        collateralAmount = _collateralAmount;
+        payoffAmount = _payoffAmount;
+        dueDate = block.timestamp + loanDuration;
+    }
+
+    event LoanPaid();
+
+    function payLoan() public payable {
+        require(block.timestamp <= dueDate);
+        require(msg.value == payoffAmount);
+
+        require(token.transfer(borrower, collateralAmount));
+        emit LoanPaid();
+        selfdestruct(payable(lender));
+    }
+
+    function repossess() public {
+        require(block.timestamp > dueDate);
+
+        require(token.transfer(lender, collateralAmount));
+        selfdestruct(payable(lender));
+    }
+}
+
+contract LoanRequest {
+    address public borrower = msg.sender;
+    ERC20 public token;
+    uint256 public collateralAmount;
+    uint256 public loanAmount;
+    uint256 public payoffAmount;
+    uint256 public loanDuration;
+
+    function LoanRequestApplication(
+        ERC20 _token,
+        uint256 _collateralAmount,
+        uint256 _loanAmount,
+        uint256 _payoffAmount,
+        uint256 _loanDuration
+    )
+        public
+    {
+        token = _token;
+        collateralAmount = _collateralAmount;
+        loanAmount = _loanAmount;
+        payoffAmount = _payoffAmount;
+        loanDuration = _loanDuration;
+    }
+
+    // Loan public loan;
+
+    event LoanRequestAccepted(address loan);
+
+    function lendEther() public payable {
+        require(msg.value == loanAmount);
+        Loan loan = Loan(
+            msg.sender,
+            borrower,
+            token,
+            collateralAmount,
+            payoffAmount,
+            loanDuration
+        );
+        require(token.transferFrom(borrower, loan, collateralAmount));
+        borrower.transfer(loanAmount);
+        emit LoanRequestAccepted(loan);
+    }
+}
